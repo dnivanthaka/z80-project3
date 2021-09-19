b4ffccf95957d0e2635295c88925074c032e9d96
Dinusha Amerasinghe <dinusha@dinushaLaptop.localdomian>
2021-09-19 23:17:32 +1000

    Refactored

---
+++ ide_cntl8255.asm
@@ -0,0 +1,187 @@
+IDECTRL_CTL   .equ IDECTRL_BASE + 0x30
+IDECTRL_PORTA .equ IDECTRL_BASE + 0
+IDECTRL_PORTB .equ IDECTRL_BASE + 0x10
+IDECTRL_PORTC .equ IDECTRL_BASE + 0x20
+
+#define ATA_A0    0x01 ;non inverted
+#define ATA_A1    0x01 ;not inverted
+#define ATA_A2    0x01 ;not inverted
+#define ATA_RD    0x01 ;inverted
+#define ATA_RW    0x01 ;inverted
+#define ATA_RESET 0x01 ;inverted
+#define ATA_CS0   0x01 ;inverted
+#define ATA_CS1   0x01 ;inverted
+
+ATA_REGDATA  .equ 0x00
+ATA_REGFEAT  .equ 0x01
+ATA_REGSECTS .equ 0x02
+ATA_REGLBA1  .equ 0x03 
+ATA_REGLBA2  .equ 0x04 
+ATA_REGLBA3  .equ 0x05 
+ATA_REGLBA4  .equ 0x06 
+ATA_REGSTAT  .equ 0x07
+
+;==================================================================================
+ata_init:
+    ;Control word 1001 0010
+    ;0x92
+    ld a, 0x92
+    out (IDECTRL_CTL), a ; DATA port as input and control port as output
+
+    ld a, 0
+    out (IDECTRL_PORTC), a ; Set control lines high - inverted output
+
+    call ata_wait_for_ready
+
+    ;setting drive as master and LBA enable LBA
+    ld a, 0x80
+    out (IDECTRL_CTL), a
+    ld a, 0
+    in a, (IDECTRL_PORTB) ; higher byte
+    ld a, 0xe0
+    in a, (IDECTRL_PORTA) ; lower byte
+    ld a, 0x56
+    out (IDECTRL_PORTC), a ; Set control lines 
+    nop
+    ld a, 0x92
+    out (IDECTRL_CTL), a ; DATA port as input and control port as output
+
+    call ata_wait_for_ready
+
+    ret
+;==================================================================================
+
+ata_wait_for_ready:
+    call ata_read_status
+    ;wait for bsy flag to clear and rdy flag to set
+    and 0b11000000
+    xor 0b01000000
+    jr nz, ata_wait_for_ready 
+
+    ret
+;==================================================================================
+
+ata_wait_for_drq:
+    call ata_read_status
+    and 0b00001000
+    xor 0b00001000
+    jr nz, ata_wait_for_drq
+
+    ret
+;==================================================================================
+
+;register to write, data
+ata_write:
+
+    ret
+
+;==================================================================================
+;register to read
+ata_read:
+
+    ret
+
+;==================================================================================
+;assumes databus in output mode, pass 2 bytes using stack 
+ata_data_write:
+    ;pop hl
+    ld hl, 2
+    add hl, sp
+    ld a, (hl)
+    inc hl    
+    ;ld (ata_data), hl
+    ;ld hl, (ata_data)
+    ;ld a, (ata_data) 
+    out (IDECTRL_PORTB), a ;low byte
+    ld a, (hl)
+    ;ld a, (ata_data + 1) 
+    out (IDECTRL_PORTA), a ; high byte
+
+    ret
+
+;==================================================================================
+;assumes databus in input mode, pass 2 parameters using stack, 1st param num words (2bytes), 2nd param pointer to buffer 
+; ata_data_read:
+;     ld hl, 2
+;     add hl, sp
+;     ld bc, (hl)    ;pointer to buffer
+;     inc hl
+;     ld de, (hl)    ;num bytes
+;     ld hl, bc 
+; ata_data_read_loop:
+;     in a, (IDECTRL_PORTB) ;upper byte 1st
+;     ld (hl), a
+;     inc hl
+;     in a, (IDECTRL_PORTA) ; lower byte 2nd
+;     ld (hl), a
+;     inc hl
+;     ld a, d
+;     call uart_hex2ascii
+;     ;ld a, e
+;     ;call uart_hex2ascii
+;     dec de
+;     jp nz, ata_data_read_loop
+
+;     ret
+
+ata_data_read:
+    push af
+    push bc
+    ld b, a                  ;index of last word to read
+    ld a, 0x92
+    out (IDECTRL_CTL), a
+
+    ld hl, ata_data
+ata_data_read_loop:
+    ld a, 0x30
+    out (IDECTRL_PORTC), a
+    in a, (IDECTRL_PORTB)
+    ld (hl), a
+    inc hl
+    in a, (IDECTRL_PORTA)
+    ld (hl), a
+    inc hl
+    ld a, 0
+    out (IDECTRL_PORTC), a
+    djnz ata_data_read_loop
+    ;reading index 0
+    ld a, 0x30
+    out (IDECTRL_PORTC), a
+    in a, (IDECTRL_PORTB)
+    ld (hl), a
+    inc hl
+    in a, (IDECTRL_PORTA)
+    ld (hl), a
+    inc hl
+    ld a, 0
+    out (IDECTRL_PORTC), a
+    pop bc
+    pop af
+
+    ret
+
+;==================================================================================
+ata_drive_reset:
+
+    ret
+
+;==================================================================================
+ata_read_status:
+    push bc 
+    ld a, 0x37
+    out (IDECTRL_PORTC), a ; Set control lines 
+    in a, (IDECTRL_PORTB) ; lower byte 2nd
+    in a, (IDECTRL_PORTA) ; lower byte 2nd
+    ld b, a
+    ld a, 0
+    out (IDECTRL_PORTC), a ; Set control lines 
+    ld a, b
+    pop bc 
+    ret
+
+;==================================================================================
+
+;ata_data  .db 0
+ata_cntrl .db 0xff
+tempdata .db 0
+tempdata2 .db 'Z' 
--- main.asm
+++ main.asm
@@ -2,31 +2,7 @@ BANKSEL   .equ 0x00
 ;ROMDIS    .equ 0x01
 IDECTRL_BASE   .equ 0x01
 
-IDECTRL_CTL   .equ IDECTRL_BASE + 0x30
-IDECTRL_PORTA .equ IDECTRL_BASE + 0
-IDECTRL_PORTB .equ IDECTRL_BASE + 0x10
-IDECTRL_PORTC .equ IDECTRL_BASE + 0x20
-
-#define ATA_A0    0x01 ;non inverted
-#define ATA_A1    0x01 ;not inverted
-#define ATA_A2    0x01 ;not inverted
-#define ATA_RD    0x01 ;inverted
-#define ATA_RW    0x01 ;inverted
-#define ATA_RESET 0x01 ;inverted
-#define ATA_CS0   0x01 ;inverted
-#define ATA_CS1   0x01 ;inverted
-
-ATA_REGDATA  .equ 0x00
-ATA_REGFEAT  .equ 0x01
-ATA_REGSECTS .equ 0x02
-ATA_REGLBA1  .equ 0x03 
-ATA_REGLBA2  .equ 0x04 
-ATA_REGLBA3  .equ 0x05 
-ATA_REGLBA4  .equ 0x06 
-ATA_REGSTAT  .equ 0x07
-
 RAM_TOP .equ 0xffff
-
 
 ata_data  .equ 0x8000
 
@@ -61,6 +37,11 @@ MAIN:
 
     call uart_init
 
+    ld hl, STR_VERSION
+    call uart_print_str
+    ld hl, STR_DETECTHDD
+    call uart_print_str
+
     ;register = status = 0x07
 
     ;ld hl, tempdata2
@@ -72,8 +53,8 @@ MAIN:
     ;ld a, (hl)
     ;call uart_putchar
 
-    ld a, 'X'
-    call uart_putchar
+    ;ld a, 'X'
+    ;call uart_putchar
 
     ;ld A, 0
     ;out (UART_OPCR), A
@@ -127,7 +108,6 @@ MAIN:
      ;call uart_output
     call ata_init
 
-
     ;ld a, 0x80
     ;out (IDECTRL_CTL), a
 
@@ -177,61 +157,171 @@ MAIN:
     ;out (IDECTRL_CTL), a
     ld a, 0x57
     out (IDECTRL_PORTC), a ; Set control lines 
-;l1:
-;    jr l1
     ;ld a, 0x92
     ;out (IDECTRL_CTL), a
-wait_for_drq:
-     call ata_read_status
-     and 0b00001000
-     xor 0b00001000
-     jr nz, wait_for_drq
+    call ata_wait_for_drq
     ;call uart_hex2ascii
     ;read data register
-    ld a, 0x92
-    out (IDECTRL_CTL), a
+;     ld a, 0x92
+;     out (IDECTRL_CTL), a
+    ld a, 0xff
+    call ata_data_read
 
-    ld b, 0xff
-    ld hl, ata_data
-test_loop:
-    ld a, 0x30
-    out (IDECTRL_PORTC), a
-    in a, (IDECTRL_PORTB)
-    ;ld a, 0x55
-    ld (hl), a
-    inc hl
-    in a, (IDECTRL_PORTA)
-    ;ld a, 0xaa
-    ld (hl), a
-    inc hl
-    ld a, 0
-    out (IDECTRL_PORTC), a
-    ;call uart_putchar
-    ;call ata_read_status
-    ;call uart_hex2ascii
-    ;ld a, ' '
-    ;call uart_putchar
-    djnz test_loop
+;     ld b, 0xff
+;     ld hl, ata_data
+; test_loop:
+;     ld a, 0x30
+;     out (IDECTRL_PORTC), a
+;     in a, (IDECTRL_PORTB)
+;     ;ld a, 0x55
+;     ld (hl), a
+;     inc hl
+;     in a, (IDECTRL_PORTA)
+;     ;ld a, 0xaa
+;     ld (hl), a
+;     inc hl
+;     ld a, 0
+;     out (IDECTRL_PORTC), a
+;     ;call uart_putchar
+;     ;call ata_read_status
+;     ;call uart_hex2ascii
+;     ;ld a, ' '
+;     ;call uart_putchar
+;     djnz test_loop
 
 
     ld a, 0x92
     out (IDECTRL_CTL), a
     ;ld a, 0x80
     ;out (IDECTRL_CTL), a
+    ld hl, ata_data+54
+    call uart_print_str
+    ld a, 13
+    call uart_putchar
+    ld a, 10
+    call uart_putchar
 
+    call ata_wait_for_ready
     
-    ld b, 0xff
+    ;reading 0 sector
+    
+    ;setting LBA addresses
+    ld a, 0x80
+    out (IDECTRL_CTL), a
+    ld a, 0
+    out (IDECTRL_PORTB), a
+    ld a, 0x00
+    out (IDECTRL_PORTA), a
+    ;set command register write
+    ld a, 0x53
+    out (IDECTRL_PORTC), a ; Set control lines 
+    ;ld a, 0
+    ;out (IDECTRL_PORTC), a ; Set control lines 
+    
+    call ata_wait_for_ready
+    
+    ;setting LBA addresses
+    ld a, 0x80
+    out (IDECTRL_CTL), a
+    ld a, 0
+    out (IDECTRL_PORTB), a
+    ld a, 0x00
+    out (IDECTRL_PORTA), a
+    ;set command register write
+    ld a, 0x54
+    out (IDECTRL_PORTC), a ; Set control lines 
+    ;ld a, 0
+    ;out (IDECTRL_PORTC), a ; Set control lines 
+    
+    call ata_wait_for_ready
+    
+    ;setting LBA addresses
+    ld a, 0x80
+    out (IDECTRL_CTL), a
+    ld a, 0
+    out (IDECTRL_PORTB), a
+    ld a, 0x00
+    out (IDECTRL_PORTA), a
+    ;set command register write
+    ld a, 0x55
+    out (IDECTRL_PORTC), a ; Set control lines 
+    ;ld a, 0
+    ;out (IDECTRL_PORTC), a ; Set control lines 
+    
+    call ata_wait_for_ready
+
+    ;setting LBA addresses
+    ld a, 0x80
+    out (IDECTRL_CTL), a
+    ld a, 0
+    out (IDECTRL_PORTB), a
+    ld a, 0xe0
+    out (IDECTRL_PORTA), a
+    ;set command register write
+    ld a, 0x56
+    out (IDECTRL_PORTC), a ; Set control lines 
+    ;ld a, 0
+    ;out (IDECTRL_PORTC), a ; Set control lines 
+    
+    call ata_wait_for_ready
+    
+    ;setting sector count
+    ld a, 0x80
+    out (IDECTRL_CTL), a
+    ld a, 0
+    out (IDECTRL_PORTB), a
+    ld a, 0x01
+    out (IDECTRL_PORTA), a
+    ;set command register write
+    ld a, 0x52
+    out (IDECTRL_PORTC), a ; Set control lines 
+    ;ld a, 0
+    ;out (IDECTRL_PORTC), a ; Set control lines 
+
+    call ata_wait_for_ready
+    
+    ;send command 0x20
+    ld a, 0x80
+    out (IDECTRL_CTL), a
+    ld a, 0
+    out (IDECTRL_PORTB), a
+    ld a, 0x20
+    out (IDECTRL_PORTA), a
+    ;set command register write
+    ld a, 0x57
+    out (IDECTRL_PORTC), a ; Set control lines 
+    ;ld a, 0
+    ;out (IDECTRL_PORTC), a ; Set control lines 
+
+;l1:
+    ;call ata_read_status
+    ;call uart_hex2ascii
+    ;jr l1 
+    call ata_wait_for_drq
+
+    ld a, 0xff
+    call ata_data_read
+
+    ld b, 0xff     ;index of last word
     ld hl, ata_data
 test_print:
+    ;print offset value
+    ;ld a, b
+    ;call uart_hex2ascii
     ld a, (hl)
-    call uart_putchar
+    call uart_hex2ascii
     inc hl
     ld a, (hl)
-    call uart_putchar
+    call uart_hex2ascii
     inc hl
-    ;ld a, ' '
-    ;call uart_putchar
+    ld a, 0x10 
+    call uart_putchar
     djnz test_print
+
+;l1:
+    ;call ata_read_status
+    ;call uart_hex2ascii
+    ;jr l1 
 
     ld a, 'D'
     call uart_putchar
@@ -318,132 +408,11 @@ delay_ms_lp:
     pop af
     ret
 
-ata_init:
-    ;Control word 1001 0010
-    ;0x92
-    ld a, 0x92
-    out (IDECTRL_CTL), a ; DATA port as input and control port as output
-
-    ld a, 0
-    out (IDECTRL_PORTC), a ; Set control lines high - inverted output
-
-    ;reset drive
-    ;ld a, (1 < 7)
-    ;ld a, 0x80
-    ;out (IDECTRL_PORTC), a ; Set control lines high - inverted output
-
-    ;call DELAY_MS
-    ;call DELAY_MS
-
-    ;ld a, 0
-    ;out (IDECTRL_PORTC), a ; Set control lines high - inverted output
-    
-    ;call DELAY_MS
-    ;call DELAY_MS
-    ;call DELAY_MS
-    ;call DELAY_MS
-
-ata_init_ready_wait:
-    call ata_read_status
-    ;wait for bsy flag to clear and rdy flag to set
-    and 0b11000000
-    xor 0b01000000
-    jr nz, ata_init_ready_wait 
-
-    ;setting drive as master and LBA enable LBA
-    ld a, 0x80
-    out (IDECTRL_CTL), a
-    ld a, 0
-    in a, (IDECTRL_PORTB) ; higher byte
-    ld a, 0xe0
-    in a, (IDECTRL_PORTA) ; lower byte
-    ld a, 0x56
-    out (IDECTRL_PORTC), a ; Set control lines 
-    nop
-    ld a, 0x92
-    out (IDECTRL_CTL), a ; DATA port as input and control port as output
-
-ata_init_ready_wait2:
-    call ata_read_status
-    ;wait for bsy flag to clear and rdy flag to set
-    and 0b11000000
-    xor 0b01000000
-    jr nz, ata_init_ready_wait2 
-
-    ret
-
-;register to write, data
-ata_write:
-
-    ret
-
-;register to read
-ata_read:
-
-    ret
-
-;assumes databus in output mode, pass 2 bytes using stack 
-ata_data_write:
-    ;pop hl
-    ld hl, 2
-    add hl, sp
-    ld a, (hl)
-    inc hl    
-    ;ld (ata_data), hl
-    ;ld hl, (ata_data)
-    ;ld a, (ata_data) 
-    out (IDECTRL_PORTB), a ;low byte
-    ld a, (hl)
-    ;ld a, (ata_data + 1) 
-    out (IDECTRL_PORTA), a ; high byte
-
-    ret
-
-;assumes databus in input mode, pass 2 parameters using stack, 1st param num words (2bytes), 2nd param pointer to buffer 
-ata_data_read:
-    ld hl, 2
-    add hl, sp
-    ld bc, (hl)    ;pointer to buffer
-    inc hl
-    ld de, (hl)    ;num bytes
-    ld hl, bc 
-ata_data_read_loop:
-    in a, (IDECTRL_PORTB) ;upper byte 1st
-    ld (hl), a
-    inc hl
-    in a, (IDECTRL_PORTA) ; lower byte 2nd
-    ld (hl), a
-    inc hl
-    ld a, d
-    call uart_hex2ascii
-    ;ld a, e
-    ;call uart_hex2ascii
-    dec de
-    jp nz, ata_data_read_loop
-
-    ret
-
-ata_drive_reset:
-
-    ret
-
-ata_read_status:
-    push bc 
-    ld a, 0x37
-    out (IDECTRL_PORTC), a ; Set control lines 
-    in a, (IDECTRL_PORTB) ; lower byte 2nd
-    in a, (IDECTRL_PORTA) ; lower byte 2nd
-    ld b, a
-    ld a, 0
-    out (IDECTRL_PORTC), a ; Set control lines 
-    ld a, b
-    pop bc 
-    ret
-
-
-;ata_data  .db 0
-ata_cntrl .db 0xff
-tempdata .db 0
-tempdata2 .db 'Z' 
+STR_VERSION .db 13, 10, "Z80 Boot BIOS v0.9, By Dinusha Amerasinghe" ,13, 10, 0
+STR_DETECTHDD .db "Detecting fixed disk ...", 13, 10, 0
+STR_FOUNDHDD .db "Found", 0
+STR_NOTFOUNDHDD .db "Not Found", 13, 10, 0
+STR_NOBOOTDEVICE .db "No boot device present", 13, 10, 0
 
 #include "uart_scc2692.asm" 
+#include "ide_cntl8255.asm"
